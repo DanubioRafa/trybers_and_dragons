8a872c48a3ac8e51b607404541ec925a6e9f9f6a
Danubio Rafael <danubio.rafaeel@gmail.com>
2022-11-23 18:15:52 -0300

    Req-10 done

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,23 @@
+import Character from '../Character';
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+class PVP extends Battle {
+  constructor(
+    protected player1: Character | Fighter,
+    protected player2: Character | Fighter,
+  ) {
+    super(player1);
+  }
+
+  fight(): number {
+    while (this.player1.lifePoints > 0 && this.player2.lifePoints > 0) {
+      this.player1.attack(this.player2);
+      this.player2.attack(this.player1);
+    }
+
+    return this.player1.lifePoints > 0 ? 1 : -1;
+  }
+}
+
+export default PVP;
