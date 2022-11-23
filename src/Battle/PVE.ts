5c22ab858f6e704eb1ffe25450c4a9c222155d49
Danubio Rafael <danubio.rafaeel@gmail.com>
2022-11-23 19:04:49 -0300

    Req-11 done

---
+++ src/Battle/PVE.ts
@@ -0,0 +1,33 @@
+import Character from '../Character';
+import Fighter, { SimpleFighter } from '../Fighter';
+import Monster from '../Monster';
+import Battle from './Battle';
+
+class PVE extends Battle {
+  constructor(
+    protected player: Character | Fighter,
+    protected monsters: (Monster | Fighter | SimpleFighter)[],
+  ) {
+    super(player);
+  }
+
+  private attacks(monster: Monster | Fighter | SimpleFighter) {
+    if (this.player.lifePoints > 0) {
+      this.player.attack(monster);
+    }
+    if (monster.lifePoints > 0) {
+      monster.attack(this.player);
+    }
+  }
+
+  fight(): number {
+    this.monsters.forEach((monster) => {
+      while (this.player.lifePoints > 0 && monster.lifePoints > 0) {
+        this.attacks(monster);
+      }
+    });
+    return this.player.lifePoints > 0 ? 1 : -1;
+  }
+}
+
+export default PVE;
